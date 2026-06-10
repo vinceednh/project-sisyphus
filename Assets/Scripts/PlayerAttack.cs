8ee1e3b7019239c6203814f367577008d1a8f49f
Athena Nelson <147112330+thena-ay@users.noreply.github.com>
2026-06-09 21:52:32 -0700

    Attack more enemy sound effects and audio to title and end screens

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -37,7 +37,7 @@ public class PlayerAttack : MonoBehaviour
         Vector3 aimDirection = GetAimDirection();
         Vector3 spawnPosition = spawnPoint.position + aimDirection * spawnForwardOffset;
 
-        AudioManager.Instance.Play(AudioManager.SoundType.Bolt);
+        AudioManager.Instance.Play(AudioManager.SoundType.Decoy);
 
         GameObject projectileObject = projectilePrefab != null
             ? Instantiate(projectilePrefab, spawnPosition, Quaternion.LookRotation(aimDirection))
